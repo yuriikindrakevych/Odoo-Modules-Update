f7efe3cd2cdf458c91df4c04aa799b8ffe70a72d
Maxym <maxym@MacBook-Pro-Maxym.local>
2026-01-16 23:29:29 +0200

    made account_dynamic_reports installable in 18 version odoo

--- account_dynamic_reports/controllers/main.py
+++ account_dynamic_reports/controllers/main.py
@@ -2,10 +2,10 @@
 
 import odoo
 from odoo import http
-from odoo.http import request
+from odoo.http import request, content_disposition
 from odoo.api import Environment
 from odoo import SUPERUSER_ID
-from odoo.addons.web.controllers.main import ensure_db
+from odoo.addons.web.controllers.utils import ensure_db
 import datetime
 import json
 import logging
@@ -14,7 +14,7 @@ from datetime import datetime
 
 # from docutils.languages import fa
 _logger = logging.getLogger(__name__)
-from odoo.addons.web.controllers.main import Home, serialize_exception, content_disposition
+from odoo.addons.web.controllers.home import Home
 from odoo.tools.translate import _
 import base64
 
@@ -23,7 +23,6 @@ class Binary(http.Controller):
     """Common controller to download file"""
 
     @http.route('/web/binary/download_document', type='http', auth="public")
-    @serialize_exception
     def download_document(self, model, field, id, filename=None, **kw):
         env = Environment(request.cr, SUPERUSER_ID, {})
         res = env[str(model)].search([('id', '=', int(id))]).sudo().read()[0]
--- account_dynamic_reports/models/res_company.py
+++ account_dynamic_reports/models/res_company.py
@@ -77,7 +77,8 @@ class ins_account_financial_report(models.Model):
         ], 'Type', default='sum')
     account_ids = fields.Many2many('account.account', 'ins_account_account_financial_report', 'report_line_id', 'account_id', 'Accounts')
     account_report_id = fields.Many2one('ins.account.financial.report', 'Report Value')
-    account_type_ids = fields.Many2many('account.account.type', 'ins_account_account_financial_report_type', 'report_id', 'account_type_id', 'Account Types')
+    # Odoo 18: account.account.type model no longer exists, account_type is now a selection on account.account
+    account_type_ids = fields.Char('Account Types', help='Comma-separated account type codes (e.g., income,expense)')
     sign = fields.Selection([('-1', 'Reverse balance sign'), ('1', 'Preserve balance sign')], 'Sign on Reports', required=True, default='1',
                             help='For accounts that are typically more debited than credited and that you would like to print as negative amounts in your reports, you should reverse the sign of the balance; e.g.: Expense account. The same applies for accounts that are typically more credited than debited and that you would like to print as positive amounts in your reports; e.g.: Income account.')
     range_selection = fields.Selection([
